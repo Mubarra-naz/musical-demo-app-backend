dda11462c63fa1109b1ab1666a3ec04897fda221
Mubarra-naz <mubarra.naz@square63.com>
2023-01-21 17:59:02 +0000

    categories CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2023_01_19_112722) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_categories_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -56,4 +64,5 @@ ActiveRecord::Schema.define(version: 2023_01_19_112722) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "categories"
 end
