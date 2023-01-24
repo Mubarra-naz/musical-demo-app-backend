c252c005708673b40babc7262b35afcba37ba1de
Mubarra-naz <mubarra.naz@square63.com>
2023-01-24 07:27:13 +0000

    updates

--- Gemfile
+++ Gemfile
@@ -28,9 +28,6 @@ gem 'devise'
 gem 'jwt'
 gem 'fast_jsonapi'
 gem "dotenv-rails"
-gem 'omniauth'
-gem 'omniauth-google-oauth2'
-gem 'omniauth-rails_csrf_protection'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.google_oauth2_data"] && session["devise.google_oauth2_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
-end
