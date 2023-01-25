f0bf4df366ac90cbb907dc60cee55bc03b3a7425
Mubarra-naz <mubarra.naz@square63.com>
2023-01-25 17:11:27 +0000

    nil check fir favourties

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,21 +2,23 @@ class Api::UsersController < ApiController
   before_action :logged_in?
 
   def add_favourite
-    favourite = Favourite.new(user_id: current_user.id, track_id: params[:track_id])
-    if favourite.save
-      render json: { message: "Added to Favourites", track_id: favourite.track_id }, status: :created
+    @favourite = Favourite.new(user_id: current_user.id, track_id: params[:track_id])
+    if @favourite.save
+      render json: { message: "Added to Favourites", track_id: @favourite.track_id }, status: :created
     else
-      render json: { message: "#{favourite.errors.full_messages.to_sentence}" }, status: :unprocessable_entity
+      render json: { message: "#{@favourite.errors.full_messages.to_sentence}" }, status: :unprocessable_entity
     end
   end
 
   def remove_favourite
-    favourite = current_user.favourites.find_by(track_id: params[:track_id])
+    return render json: { message: "User doesn't have any favourites yet." } unless current_user.favourites.present?
 
-    if favourite.destroy
+    @favourite = current_user.favourites.find_by(track_id: params[:track_id])
+
+    if @favourite.destroy
       render json: { message: "Removed from favourites", track_id: params[:track_id] }, status: :ok
     else
-      render json: { error: "#{favourite.errors.full_messages.to_sentence}" }, status: :unprocessable_entity
+      render json: { error: "#{@favourite.errors.full_messages.to_sentence}" }, status: :unprocessable_entity
     end
   end
 end
